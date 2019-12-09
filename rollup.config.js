9a9b98b6e8cbb96efe74a67a07118ce24fed75c6
Justin Willis <jgw9617@gmail.com>
2019-12-09 09:33:31 -0800

    build(): remove console logs during build

--- rollup.config.js
+++ rollup.config.js
@@ -2,17 +2,21 @@
 import resolve from 'rollup-plugin-node-resolve';
 import minify from 'rollup-plugin-babel-minify';
 import minifyHTML from 'rollup-plugin-minify-html-literals';
+import strip from '@rollup/plugin-strip';
 
 export default {
-	input: ['build/pwa-install.js'],
-	output: {
-		file: 'dist/pwa-install.js',
+  input: ['build/pwa-install.js'],
+  output: {
+    file: 'dist/pwa-install.js',
     format: 'es',
-		sourcemap: true
-	},
-	plugins: [
+    sourcemap: true
+  },
+  plugins: [
     resolve(),
     minifyHTML(),
-    minify()
+    minify(),
+    strip({
+      functions: ['console.log']
+    })
   ]
 };
